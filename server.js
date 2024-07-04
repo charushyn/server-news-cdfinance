4ebfe4206596cf4c2700e8b4257f3a63fb1a8bd8
Illia Charushyn <charushyngg@gmail.com>
2024-07-04 21:27:16 +0200

    img public dir
    .

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import multer from 'multer'
 import { createHmac } from 'crypto'
 import cookieParser from "cookie-parser"
 import sharp from 'sharp'
+import { Blob } from 'buffer'
 
 // routes
 import routeServices from './routes/editor/services.js'
@@ -23,24 +24,10 @@ configDotenv();
 const app = express()
 const PORT = 1488
 
-const imageUploadPath = process.env.PATH_TO_IMAGES;
-
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, imageUploadPath)
-  },
-  filename: function(req, file, cb) {
-    cb(null, `${file.fieldname}_dateVal_${Date.now()}_${file.originalname}`)
-  }
-})
-
-const imageUpload = multer({storage: storage})
-
 app.use(cors({
     origin: ['http://localhost:3000', 'https://localhost:3000', 'http://127.0.0.1:3000', 'https://127.0.0.1:3000', 'http://cdfinance.pl', 'https://cdfinance.pl'],
     credentials: true
 }))
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
@@ -54,39 +41,7 @@ app.use('', opinionsRouter)
 app.use('', faqsRouter)
 app.use('', footerRouter)
 app.use('', whywesRouter)
-
-
-app.post('/img', imageUpload.none(), (req, res) => {
-  fs.readFile(`${imageUploadPath}/${req.body.name}`, (err, fileData) => {
-    if(err){return res.send(JSON.stringify(err)).status(500)}
-
-    const img = sharp(fileData)
-    const string = req.body.name.split('.')[1]
-
-    if(string === 'svg'){
-        img
-        .toBuffer()
-        .then(function (data) {
-        let base64Encoded = data.toString("base64");
-        const url = `data:image/svg+sml;base64,${base64Encoded}`;
-
-        res.status(200).send({ data: url });
-        });
-    } else {
-        img
-        .jpeg({ mozjpeg: true }) 
-        .toBuffer()
-        .then(function (data) {
-          let base64Encoded = data.toString("base64");
-          const url = `data:image/jpeg;base64,${base64Encoded}`;
-    
-          res.status(200).send({ data: url });
-        });
-    }
-  }) 
-})
-
-
+app.use('/img', express.static(`${process.env.PATH_TO_IMAGES}`))
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
